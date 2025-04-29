413cb7908f4d44a95a562c3602bf884cc109a3b4
x0rw <mahdi.svt5@gmail.com>
2025-04-29 03:12:48 +0100

    [Sphinx Extension] Add check for required guideline fields (#45)
    * Add check for required guideline fields
    
    * Remove debugging output
    
    * Apply suggestions from code review
    
    Co-authored-by: Pete LeVasseur <plevasseur@gmail.com>
    
    * test: intentionally break build to verify CI failure
    
    this commit removes tags, decidability, scope fields from types-and-traits.rst
    
    * Revert temporary build failure for validation test
    
    ---------
    
    Co-authored-by: Pete LeVasseur <plevasseur@gmail.com>

---
+++ exts/coding_guidelines/guidelines_checks.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: MIT OR Apache-2.0
+# SPDX-FileCopyrightText: The Coding Guidelines Subcommittee Contributors
+
+from sphinx.errors import SphinxError
+from sphinx_needs.data import SphinxNeedsData
+import logging
+
+logger = logging.getLogger('sphinx')
+
+class IntegrityCheckError(SphinxError):
+    category = "Integrity Check Error"
+
+def validate_required_fields(app, env):
+    """
+    Validate the required fields defined in conf.py 
+    """
+    logger.debug("Validating required fields")
+    data = SphinxNeedsData(env)
+    needs = data.get_needs_view()
+
+    required_fields = app.config.required_guideline_fields  # Access the configured values
+
+    for key, value in needs.items():
+        if value.get('type') == 'guideline':
+            missing_fields = []
+            for field in required_fields:
+                if value.get(field) in  (None, '', []):
+                    missing_fields.append(field)
+
+            if missing_fields:
+                error_message = (
+                    f"Guideline '{value.get('title')}' (ID: {value.get('id')}) "
+                    f"in {value.get('docname')}:{value.get('lineno')} is missing the following required fields: "
+                    f"{', '.join(missing_fields)}"
+                )
+                logger.error(error_message)
+                app.builder.statuscode = 1
+                raise IntegrityCheckError(error_message) 
+            logger.info("No missing required field")
